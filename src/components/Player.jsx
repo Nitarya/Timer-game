187857e3869b86436702841faa656eb424e01a34
Nitya <niittss27@gmail.com>
2023-12-15 15:37:37 +0530

    Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,16 @@
 export default function Player() {
+
+const playerName = useRef()
+const [enteredPlayerName, setEnteredPlayerName] = useState(null)
+function handleClick() {
+  setEnteredPlayerName (playerName.current.value)
+}
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'} </h2>
       <p>
-        <input type="text" />
+        <input type="text" ref={playerName}/>
         <button>Set Name</button>
       </p>
     </section>
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,9 @@
+function TimerChallenge() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TimerChallenge
